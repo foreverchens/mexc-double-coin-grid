3014f8555b5726d1db7a9056ca41ec0587ba6824
ychen5325 <q1416349095@gmail.com>
2023-08-10 21:13:18 +0800

    增加交易日志输出

--- src/main/java/com/grid/mxc/Main.java
+++ src/main/java/com/grid/mxc/Main.java
@@ -44,7 +44,8 @@ public class Main {
 
 
 	static {
-		InputStream resourceAsStream = MxcClient.class.getResourceAsStream("/application-grid.yaml");
+		InputStream resourceAsStream = MxcClient.class.getResourceAsStream("/application-grid" +
+				".yaml");
 		Properties properties = new Properties();
 		try {
 			properties.load(resourceAsStream);
@@ -54,14 +55,16 @@ public class Main {
 		symbolA = properties.getProperty("symbolA");
 		symbolB = properties.getProperty("symbolB");
 		swapQtyOfA = new BigDecimal(properties.getProperty("swapQtyOfA"));
-		gridRate = new BigDecimal(properties.getProperty("gridRate")).multiply(new BigDecimal("0.01"));
+		gridRate =
+				new BigDecimal(properties.getProperty("gridRate")).multiply(new BigDecimal("0" +
+						".01"));
 		tradeDepth = Integer.parseInt(properties.getProperty("tradeDepth", "1"));
 	}
 
 	public static void init() throws Exception {
 		List<String> supSymbols = MxcClient.getSupSymbols();
 		if (!supSymbols.contains(symbolA) || !supSymbols.contains(symbolB)) {
-			throw new RuntimeException("该交易对不支持api交易,code=10007,自检地址\\https://www.mexc.com/zh-CN/user/openapi");
+			throw new RuntimeException("该交易对不支持api交易,code=10007,自检地址\\https://www.mexc" + ".com" + "/zh" + "-CN/user/openapi");
 		}
 
 		// 合理价格及相关汇率
@@ -89,33 +92,34 @@ public class Main {
 				PriceBook priceBookB = MxcClient.getPriceBook(symbolB);
 				// 检查是否满足汇率卖出条件、你可能需要卖出A、然后买入B、
 				// 所以你的汇率应该用A的最优买单价格和B的最优卖单价格计算。
-				BigDecimal curSellRatio = priceBookA.getBidPrice().divide(priceBookB.getAskPrice(), 8,
-						RoundingMode.DOWN);
+				BigDecimal curSellRatio = priceBookA.getBidPrice().divide(priceBookB.getAskPrice()
+						, 8, RoundingMode.DOWN);
 				log.info("  [{} < {} < {}]", buyRatio, curSellRatio, sellRatio);
 				if (curSellRatio.compareTo(sellRatio) > 0) {
-					log.info("  curSellRatio:{} > sellRatio:{} sell A buy B", curSellRatio, sellRatio);
+					log.info("	当前汇率高于卖出汇率、卖A买B");
+
 					// 卖A买B、
 					exeSellA(priceBookA, priceBookB);
 					// 更新买/卖汇率
 					sellRatio = curSellRatio.add(curSellRatio.multiply(gridRate));
 					buyRatio = curSellRatio.subtract(curSellRatio.multiply(gridRate));
-					log.info("  new sellRatio:{} ,new buyRatio:{}", sellRatio, buyRatio);
+					log.info("	完成卖A买B:最新买入汇率:{},卖出汇率:{}", buyRatio, sellRatio);
 					continue;
 				}
 
 				// 检查是否满足汇率买入条件、你可能需要卖出B、然后买入A、
 				// 所以你的汇率应该用B的最优买单价格和A的最优卖单价格计算。
-				BigDecimal curBuyRatio = priceBookA.getAskPrice().divide(priceBookB.getBidPrice(), 8,
-						RoundingMode.DOWN);
+				BigDecimal curBuyRatio = priceBookA.getAskPrice().divide(priceBookB.getBidPrice(),
+						8, RoundingMode.DOWN);
 				log.info("	[{} < {} < {}]", buyRatio, curBuyRatio, sellRatio);
 				if (curBuyRatio.compareTo(buyRatio) < 0) {
-					log.info("	curBuyRatio:{} < buyRatio:{},sell B buy A ", curBuyRatio, buyRatio);
+					log.info("	当前汇率低于买入汇率、卖B买A");
 					// 卖B买A
 					exeSellB(priceBookA, priceBookB);
 					// 更新买/卖汇率
 					sellRatio = curBuyRatio.add(curBuyRatio.multiply(gridRate));
 					buyRatio = curBuyRatio.subtract(curBuyRatio.multiply(gridRate));
-					log.info("	new sellRatio:{} ,new buyRatio:{}", sellRatio, buyRatio);
+					log.info("	完成卖B买A:最新买入汇率:{},卖出汇率:{}", buyRatio, sellRatio);
 				}
 				TimeUnit.SECONDS.sleep(30);
 			} catch (Exception ex) {
@@ -149,18 +153,23 @@ public class Main {
 			String orderId = MxcClient.createOrder(param);
 			Order order = MxcClient.getOrder(symbolA, orderId);
 			// 累计总盈利
-			sellACumQuoteQty = sellACumQuoteQty.add(new BigDecimal(order.getCummulativeQuoteQty()));
+			sellACumQuoteQty =
+					sellACumQuoteQty.add(new BigDecimal(order.getCummulativeQuoteQty()));
 			// 剩余待卖数量
 			sellAQty = sellAQty.subtract(new BigDecimal(order.getExecutedQty()));
 			// 更新最优挂单
 			priceBookA = MxcClient.getPriceBook(symbolA);
-			log.info("	待卖数量:{},本次卖出数量:{},卖出价格:{},卖出金额:{},卖出总盈利:{},剩余待卖:{}", swapQtyOfA, order.getExecutedQty(),
-					order.getPrice(), order.getCummulativeQuoteQty(), sellACumQuoteQty, sellAQty);
+			log.info("	本次A待卖数量:{},卖出数量:{},卖出价格:{},卖出金额:{},卖出总盈利:{},剩余待卖:{}", swapQtyOfA,
+					order.getExecutedQty(), order.getPrice(), order.getCummulativeQuoteQty(),
+					sellACumQuoteQty, sellAQty);
 		}
+		BigDecimal sellAPrice = sellACumQuoteQty.divide(swapQtyOfA.subtract(sellAQty), 1, 1);
+		log.warn("	卖A总结: 以{}的均价卖出{}个、总金额为:{}", sellAPrice.toPlainString(),
+				swapQtyOfA.subtract(sellAQty), sellACumQuoteQty);
 		// 买B的资金来源于卖A的盈利/USD
 		BigDecimal buyBOrigQuoteQty = sellACumQuoteQty;
-		log.info("	卖A累计获得:{},开始买B", buyBOrigQuoteQty);
 		x = 0;
+		// B的累计买入量
 		BigDecimal buyBCumQty = BigDecimal.ZERO;
 		while (x++ < tradeDepth && buyBOrigQuoteQty.compareTo(BigDecimal.TEN) > 0) {
 			// 获取最优一格卖单
@@ -171,7 +180,6 @@ public class Main {
 			BigDecimal orderQty = maxExeQty.compareTo(askQty) > 0 ? askQty : maxExeQty;
 			OrderParam param =
 					OrderParam.builder().symbol(symbolB).side(SideTypeEnum.BUY).type(OrderTypeEnum.IMMEDIATE_OR_CANCEL).quantity(orderQty.toString()).price(askPrice.toString()).build();
-
 			String orderId = MxcClient.createOrder(param);
 			Order order = MxcClient.getOrder(symbolB, orderId);
 			// 订单成交金额
@@ -181,12 +189,12 @@ public class Main {
 			buyBCumQty = buyBCumQty.add(new BigDecimal(order.getExecutedQty()));
 			// 更新最优挂单
 			priceBookB = MxcClient.getPriceBook(symbolB);
-			log.info("	本次买入花费:{},买入数量:{},买入价格:{},剩余可用余额:{}", order.getCummulativeQuoteQty(), order.getExecutedQty(),
-					order.getPrice(), buyBOrigQuoteQty);
+			log.info("	本次B买入花费:{},买入数量:{},买入价格:{},剩余可用余额:{}", order.getCummulativeQuoteQty(),
+					order.getExecutedQty(), order.getPrice(), buyBOrigQuoteQty);
 		}
-		log.info("	买B累计花费:{}，累计买入数量:{},平均买入价格:{},留存USD余额:{}", sellACumQuoteQty.subtract(buyBOrigQuoteQty), buyBCumQty
-				, (sellACumQuoteQty.subtract(buyBOrigQuoteQty)).divide(buyBCumQty, 8, 1), buyBOrigQuoteQty);
-
+		log.warn("	买B总结: 以{}的均价买入{}个、总金额为:{},留存USD为:{}",
+				(sellACumQuoteQty.subtract(buyBOrigQuoteQty)).divide(buyBCumQty, 8, 1), buyBCumQty
+				, sellACumQuoteQty.subtract(buyBOrigQuoteQty), buyBOrigQuoteQty);
 	}
 
 	private static void exeSellB(PriceBook priceBookA, PriceBook priceBookB) throws Exception {
@@ -200,25 +208,32 @@ public class Main {
 			BigDecimal bidQty = priceBookB.getBidQty();
 			// 最优买价的可卖数量
 			BigDecimal exeQty = sellBQty.compareTo(bidQty) > 0 ? bidQty : sellBQty;
-			OrderParam param =
-					OrderParam.builder().symbol(symbolB).side(SideTypeEnum.SELL).type("IMMEDIATE_OR_CANCEL").quantity(exeQty.toString()).price(bidPrice.toString()).build();
+			OrderParam param = OrderParam.builder().symbol(symbolB).side(SideTypeEnum.SELL).type(
+					"IMMEDIATE_OR_CANCEL").quantity(exeQty.toString()).price(bidPrice.toString()).build();
 			log.info("param:{}", JSON.toJSONString(param));
 			String orderId = MxcClient.createOrder(param);
 			Order order = MxcClient.getOrder(symbolB, orderId);
+			log.info("order:" + JSON.toJSONString(order));
 			// 累计总盈利
-			sellBCumQuoteQty = sellBCumQuoteQty.add(new BigDecimal(order.getCummulativeQuoteQty()));
+			sellBCumQuoteQty =
+					sellBCumQuoteQty.add(new BigDecimal(order.getCummulativeQuoteQty()));
 			// 剩余待卖数量
 			sellBQty = sellBQty.subtract(new BigDecimal(order.getExecutedQty()));
 			// 更新最优挂单
 			priceBookB = MxcClient.getPriceBook(symbolB);
-			log.info("  待卖数量:{},本次卖出数量:{},卖出价格:{},卖出金额:{},卖出总盈利:{},剩余待卖:{}", eqQtyOfB, order.getExecutedQty(),
-					order.getPrice(), order.getCummulativeQuoteQty(), sellBCumQuoteQty, sellBQty);
+			log.info("  本次B待卖数量:{},卖出数量:{},卖出价格:{},卖出金额:{},卖出总盈利:{},剩余待卖:{}", eqQtyOfB,
+					order.getExecutedQty(), order.getPrice(), order.getCummulativeQuoteQty(),
+					sellBCumQuoteQty, sellBQty);
 		}
+
+		BigDecimal sellBPrice = sellBCumQuoteQty.divide(swapQtyOfA.subtract(sellBQty), 1, 1);
+		log.warn("	卖B总结: 以{}的均价卖出{}个、总金额为:{}", sellBPrice.toPlainString(),
+				eqQtyOfB.subtract(sellBQty), sellBCumQuoteQty);
 		// 买B的资金来源于卖A的盈利/USD
 		BigDecimal buyAOrigQuoteQty = sellBCumQuoteQty;
-		log.info("  卖B累计获得:{}", buyAOrigQuoteQty);
-
 		x = 0;
+		// A的累计买入量
+		BigDecimal buyACumQty = BigDecimal.ZERO;
 		while (x++ < tradeDepth && buyAOrigQuoteQty.compareTo(BigDecimal.TEN) > 0) {
 			// 获取最优一格卖单
 			BigDecimal askPrice = priceBookA.getAskPrice();
@@ -226,20 +241,25 @@ public class Main {
 			// 最优卖价的最大可买数量
 			BigDecimal maxExeQty = buyAOrigQuoteQty.divide(askPrice, 8, RoundingMode.DOWN);
 			BigDecimal orderQty = maxExeQty.compareTo(askQty) > 0 ? askQty : maxExeQty;
-			OrderParam param =
-					OrderParam.builder().symbol(symbolA).side(SideTypeEnum.BUY).type("IMMEDIATE_OR_CANCEL").quantity(orderQty.toString()).price(askPrice.toString()).build();
-
+			OrderParam param = OrderParam.builder().symbol(symbolA).side(SideTypeEnum.BUY).type(
+					"IMMEDIATE_OR_CANCEL").quantity(orderQty.toString()).price(askPrice.toString()).build();
+			log.info("买A param:{}", JSON.toJSONString(param));
 			String orderId = MxcClient.createOrder(param);
 			Order order = MxcClient.getOrder(symbolA, orderId);
 			// 订单成交金额
 			String quoteQty = order.getCummulativeQuoteQty();
 			// 更新可用余额
 			buyAOrigQuoteQty = buyAOrigQuoteQty.subtract(new BigDecimal(quoteQty));
+
+			buyACumQty = buyACumQty.add(new BigDecimal(order.getExecutedQty()));
 			// 更新最优挂单
 			priceBookA = MxcClient.getPriceBook(symbolA);
-			log.info("  本次买入花费:{},买入数量:{},买入价格:{},剩余可用余额:{}", order.getCummulativeQuoteQty(), order.getExecutedQty(),
-					order.getPrice(), buyAOrigQuoteQty);
+			log.info("  本次A买入花费:{},买入数量:{},买入价格:{},剩余可用余额:{}", order.getCummulativeQuoteQty(),
+					order.getExecutedQty(), order.getPrice(), buyAOrigQuoteQty);
 		}
+		log.warn("	买A总结: 以{}的均价买入{}个、总金额为:{},留存USD为:{}",
+				(sellBCumQuoteQty.subtract(buyAOrigQuoteQty)).divide(buyACumQty, 8, 1), buyACumQty
+				, sellBCumQuoteQty.subtract(buyAOrigQuoteQty), buyAOrigQuoteQty);
 	}
 
 
